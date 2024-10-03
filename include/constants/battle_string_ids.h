f0821ca22f4d5f9165b81fbc1fb786cda72e4686
CyanSMP64 <jmleuschke64@yahoo.com>
2024-10-03 15:27:14 +1300

    multi hit plural handling

--- include/constants/battle_string_ids.h
+++ include/constants/battle_string_ids.h
@@ -381,8 +381,9 @@
 #define STRINGID_MIST                       381
 #define STRINGID_PLAYERLOSTTOENEMYTRAINER   382
 #define STRINGID_PLAYERPAIDPRIZEMONEY       383
+#define STRINGID_HITXTIME                   384
 
-#define BATTLESTRINGS_COUNT                 384
+#define BATTLESTRINGS_COUNT                 385
 
 // This is the string id that gBattleStringsTable starts with.
 // String ids before this (e.g. STRINGID_INTROMSG) are not in the table,
