c60d0a254a109c9106a59dd5dea43190810257fb
CyanSMP64 <jmleuschke64@yahoo.com>
2025-05-29 03:16:43 +1200

    start doing the thing

--- include/constants/songs.h
+++ include/constants/songs.h
@@ -543,6 +543,14 @@
 #define PH_NURSE_HELD               608
 #define PH_NURSE_SOLO               609
 
+#define MUS_TEST_1               610
+#define MUS_TEST_2               611
+#define MUS_TEST_3               612
+#define MUS_TEST_4               613
+#define MUS_TEST_5               614
+
+#define MUSIC_END       MUS_TEST_5
+
 #define MUS_ROUTE118                0x7FFF  // Map is split into 2 music sections. controlled by GetCurrLocationDefaultMusic().
 
 #define MUS_NONE                    0xFFFF
