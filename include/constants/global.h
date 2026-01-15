92fe145f50bcd7a26e20fe9cfb54a5406333ea4c
CyanSMP64 <jmleuschke64@yahoo.com>
2026-01-15 14:08:29 +1300

    use gen 9 icons from pokecommunity

--- include/constants/global.h
+++ include/constants/global.h
@@ -33,7 +33,7 @@
 #define NATDEX_VERSION_MINOR 1
 #define NATDEX_VERSION_PATCH 3
 // revision number: git describe --tags
-#define NATDEX_VERSION_BUILD 189
+#define NATDEX_VERSION_BUILD 190
 
 // party sizes
 #define PARTY_SIZE 6
