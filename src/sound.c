cb90172d97904c29dc8e759bd4a1fbc14b2a1914
CyanSMP64 <jmleuschke64@yahoo.com>
2026-03-18 16:15:23 +1300

    mossdeep gym npc no longer counts egg as pokemon

--- src/sound.c
+++ src/sound.c
@@ -52,6 +52,7 @@ static const struct Fanfare sFanfares[] = {
     [FANFARE_OBTAIN_B_POINTS]     = { MUS_OBTAIN_B_POINTS,     313 },
     [FANFARE_OBTAIN_SYMBOL]       = { MUS_OBTAIN_SYMBOL,       318 },
     [FANFARE_REGISTER_MATCH_CALL] = { MUS_REGISTER_MATCH_CALL, 135 },
+    [FANFARE_UTVANISH]            = { SE_UTVANISH,              90 },
 };
 
 void InitMapMusic(void)
