720b384ab02d9ae6eb9c8a2e21e50956afb2b75e
CyanSMP64 <jmleuschke64@yahoo.com>
2026-03-30 16:19:41 +1300

    red crit text

--- include/constants/global.h
+++ include/constants/global.h
@@ -33,7 +33,7 @@
 #define NATDEX_VERSION_MINOR 1
 #define NATDEX_VERSION_PATCH 3
 // revision number: git describe --tags
-#define NATDEX_VERSION_BUILD 239
+#define NATDEX_VERSION_BUILD 240
 
 // party sizes
 #define PARTY_SIZE 6
