961b5f27e77263c9e89097aa3d2b66f36d69fddd
CyanSMP64 <jmleuschke64@yahoo.com>
2026-05-12 00:27:13 +1200

    improve tempo scaling

--- src/m4a.c
+++ src/m4a.c
@@ -6,7 +6,7 @@ extern const u8 gCgb3Vol[];
 #define BSS_CODE __attribute__((section(".bss.code")))
 
 BSS_CODE ALIGNED(4) char SoundMainRAM_Buffer[0xa84] = {0};
-BSS_CODE ALIGNED(4) u32 hq_buffer_ptr[0x210] = {0};
+BSS_CODE ALIGNED(4) u32 hq_buffer_ptr[0x160] = {0};
 
 COMMON_DATA struct SoundInfo gSoundInfo = {0};
 COMMON_DATA struct PokemonCrySong gPokemonCrySongs[MAX_POKEMON_CRIES] = {0};
@@ -51,6 +51,32 @@ static void UNUSED UnusedDummyFunc(void)
 {
 }
 
+static void PrimeTempoCounter(struct MusicPlayerInfo *mplayInfo)
+{
+    u32 scaledTempoStep = mplayInfo->tempoI * mplayInfo->songSpeed;
+    u16 scaledTempoStepInt = scaledTempoStep >> 10;
+    u8 scaledTempoStepFrac = (scaledTempoStep >> 2) & 0xFF;
+
+    if (scaledTempoStepInt < 150)
+    {
+        if (scaledTempoStepFrac != 0)
+        {
+            mplayInfo->tempoC = 149 - scaledTempoStepInt;
+            mplayInfo->tempoScaleFrac = 0x100 - scaledTempoStepFrac;
+        }
+        else
+        {
+            mplayInfo->tempoC = 150 - scaledTempoStepInt;
+            mplayInfo->tempoScaleFrac = 0;
+        }
+    }
+    else
+    {
+        mplayInfo->tempoC = 0;
+        mplayInfo->tempoScaleFrac = 0;
+    }
+}
+
 void MPlayContinue(struct MusicPlayerInfo *mplayInfo)
 {
     if (mplayInfo->ident == ID_NUMBER)
@@ -82,7 +108,7 @@ void m4aSoundInit(void)
     SoundInit(&gSoundInfo);
     MPlayExtender(gCgbChans);
     m4aSoundMode(SOUND_MODE_DA_BIT_8
-               | SOUND_MODE_FREQ_31536
+               | SOUND_MODE_FREQ_21024
                | (15 << SOUND_MODE_MASVOL_SHIFT)
                | (15 << SOUND_MODE_MAXCHN_SHIFT));
 
@@ -646,7 +672,7 @@ void MPlayStart(struct MusicPlayerInfo *mplayInfo, struct SongHeader *songHeader
         mplayInfo->tempoD = 150;
         mplayInfo->tempoI = 150;
         mplayInfo->tempoU = 0x100;
-        mplayInfo->tempoC = 0;
+        PrimeTempoCounter(mplayInfo);
         mplayInfo->fadeOI = 0;
 
         i = 0;
@@ -1262,7 +1288,8 @@ void m4aMPlayTempoControl(struct MusicPlayerInfo *mplayInfo, u16 tempo)
     {
         mplayInfo->ident++;
         mplayInfo->tempoU = tempo;
-        mplayInfo->tempoI = ((mplayInfo->tempoD * mplayInfo->tempoU * mplayInfo->songSpeed) >> 18);
+        mplayInfo->tempoI = ((mplayInfo->tempoD * mplayInfo->tempoU) >> 8);
+        PrimeTempoCounter(mplayInfo);
         mplayInfo->ident = ID_NUMBER;
     }
 }
