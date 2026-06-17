93ae59a1da9cccfc0897c69d9bb9ef143a2e594f
CyanSMP64 <jmleuschke64@yahoo.com>
2026-06-17 16:47:34 +1200

    sprite review SA-SH

--- include/constants/global.h
+++ include/constants/global.h
@@ -33,7 +33,7 @@
 #define NATDEX_VERSION_MINOR 1
 #define NATDEX_VERSION_PATCH 3
 // revision number: git describe --tags
-#define NATDEX_VERSION_BUILD 281
+#define NATDEX_VERSION_BUILD 282
 
 // party sizes
 #define PARTY_SIZE 6
