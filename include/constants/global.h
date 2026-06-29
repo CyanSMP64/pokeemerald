d478aba5f2d706aebbe418345aca65a40195e239
CyanSMP64 <jmleuschke64@yahoo.com>
2026-06-30 00:07:39 +1200

    new pokecommunity sprites

--- include/constants/global.h
+++ include/constants/global.h
@@ -33,7 +33,7 @@
 #define NATDEX_VERSION_MINOR 2
 #define NATDEX_VERSION_PATCH 1
 // revision number: git describe --tags
-#define NATDEX_VERSION_BUILD 0
+#define NATDEX_VERSION_BUILD 1
 
 // party sizes
 #define PARTY_SIZE 6
