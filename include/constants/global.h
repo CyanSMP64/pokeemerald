25a72da524b572370e3f99cdb16ff1bea17c75d4
CyanSMP64 <jmleuschke64@yahoo.com>
2026-08-10 20:56:31 +1200

    update sound engine and rerender ds music

--- include/constants/global.h
+++ include/constants/global.h
@@ -33,7 +33,7 @@
 #define NATDEX_VERSION_MINOR 2
 #define NATDEX_VERSION_PATCH 1
 // revision number: git describe --tags
-#define NATDEX_VERSION_BUILD 3
+#define NATDEX_VERSION_BUILD 4
 
 // party sizes
 #define PARTY_SIZE 6
