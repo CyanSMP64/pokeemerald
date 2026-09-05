d5c33d83ce2524a6894f9e18596c73fcbb477109
CyanSMP64 <jmleuschke64@yahoo.com>
2026-09-05 16:57:07 +1200

    fix present type effectiveness message

--- data/battle_scripts_1.s
+++ data/battle_scripts_1.s
@@ -1693,7 +1693,6 @@ BattleScript_EffectPresent::
 	accuracycheck BattleScript_PrintMoveMissed, ACC_CURR_MOVE
 	attackstring
 	ppreduce
-	typecalc
 	presentdamagecalculation
 
 BattleScript_EffectSafeguard::
